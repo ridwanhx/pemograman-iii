78e424902a505b7b9a45c30488ecf5563358b21d
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2025-11-19 23:37:19 +0700

    Pertemuan 07 : add column categories as array and change column casts as map

--- pertemuan-07/latihan/model/struct.go
+++ pertemuan-07/latihan/model/struct.go
@@ -9,7 +9,8 @@ type Movies struct {
 	Id string `json:"id" gorm:"column:id;primaryKey;type:uuid;default:gen_random_uuid()"`
 	Title string `json:"title" gorm:"column:title;type:varchar(100);not null"`
 	Director string `json:"director" gorm:"column:director;type:varchar(100);not null"`
-	Casts pq.StringArray `json:"casts" gorm:"column:casts;type:text[];not null"`
+	Categories pq.StringArray `json:"categories" gorm:"column:categories;type:text[];not null"`
+	Casts map[string]string `json:"casts" gorm:"column:casts;type:jsonb;not null"`
 	Synopsis string `json:"synopsis" gorm:"column:synopsis;type:text"`
 	Rating float64 `json:"rating" gorm:"column:rating;type:float;not null"`
 	ReleaseYear int `json:"release_year" gorm:"column:release_year;type:integer"`
